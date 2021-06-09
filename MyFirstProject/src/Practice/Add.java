9df5e8fd780a190505cbc1b5201c094ce4706a21
HP <HP@LAPTOP-1UQMRQB4.home>
2021-06-09 13:46:38 -0400

    added first changes

---
+++ MyFirstProject/src/Practice/Add.java
@@ -0,0 +1,19 @@
+package Practice;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i=3;
+		int j=2;
+		
+		if(i>j) {
+			System.out.println("bigger " + i);}
+		else {	
+			System.out.println("smaller " + j);
+		}
+
+	}
+
+}
---
+++ MyFirstProject/src/Practice/Second.java
@@ -0,0 +1,20 @@
+package Practice;
+
+import java.util.ArrayList;
+
+public class Second {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayList<String> ls=new ArrayList<>();
+		
+		ls.add("1");
+		ls.add("2");
+		
+		for(String a:ls) {
+		System.out.println(a);
+	}
+
+}
+}
